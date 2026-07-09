f86d63ae4ef805e5df2cbb6d4f0cded0ff07ce1a
LinkKelly <54011287+LinkGoesBowling@users.noreply.github.com>
2026-07-09 16:55:10 -0600

    List players from top to bottom instead of 10 on left and 10 on right

--- script.js
+++ script.js
@@ -160,19 +160,19 @@ async function getAvgData(season){ //uses same structure as getERAData, but with
             players.sort((a, b) => b.adjustedAvg - a.adjustedAvg);
             for (let i = 0; i < 20; i++) {
                 const ol1 = document.getElementById('playerRanks');
-                if (ol1.children.length < 10){
+                if (ol1.children.length < 20){ //change to 10 for multiple rows
                         const createRanks = document.createElement('li'); //create new li elements and add them to the ol
                         createRanks.classList.add('rank' + (i + 1)); //add class
                         createRanks.setAttribute('id', 'rank' + (i + 1)); //add id
                         ol1.appendChild(createRanks);
                 }
-                const ol2 = document.getElementById('playerRanks2');
+                /* const ol2 = document.getElementById('playerRanks2'); //commented out because i wanted one row
                 if (ol2.children.length < 10){
                         const createRanks = document.createElement('li'); //create new li elements and add them to the ol
                         createRanks.classList.add('rank' + (i + 11)); //add class
                         createRanks.setAttribute('id', 'rank' + (i + 11)); //add id
                         ol2.appendChild(createRanks);
-                }
+                } */
                 const changeRank = document.getElementById("rank" + (i + 1))
                 changeRank.textContent = players[i].player.fullName + ", AVG: " + players[i].adjustedAvg + players[i].preAdjustmentAvg;
                 if (players[i].isQualified === false && colorNonQualifiedPlayers === true){
